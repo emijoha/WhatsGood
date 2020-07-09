79b83b760a0c00bed12acf46864548e03daf6d97
Daniel Abell <da450@cairn.edu>
2020-07-08 22:27:28 -0400

    added second OMDB api call to retrieve data for each movie returned on the initial OMDB api call. SearchMovies and SavedMovies now have fully populated cards (where data from api is available). Movie model is also updated.

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -2,36 +2,45 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const movieSchema = new Schema({
-  // director: [
-  //   {
-  //     type: String,
-  //     required: false
-  //   },
-  // ],
-  // description: {
-  //   type: String,
-  //   required: false,
-  // },
-  // saved book id from GoogleBooks
   movieId: {
     type: String,
     required: true,
   },
-  image: {
+  actors: {
+    type: String,
+    required: false
+  },
+  director: {
+    type: String,
+    required: false
+  },
+  genre: {
+    type: String,
+    required: false
+  },
+  plot: {
     type: String,
     required: false,
   },
-  // link: {
-  //   type: String,
-  //   required: false
-  // },
+  rated: {
+    type: String,
+    required: false
+  },
+  released: {
+    type: String,
+    required: false
+  },
+  runtime: {
+    type: String,
+    required: false
+  },
   title: {
     type: String,
     required: false,
   },
-  year: {
+  image: {
     type: String,
-    required: false
+    required: false,
   }
 });
 
