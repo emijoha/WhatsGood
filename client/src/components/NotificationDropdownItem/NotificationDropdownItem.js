ad390aa8936858f8b6bf3592439453fd2ddc960c
rpounceyjr <rpounceyjr@gmail.com>
2020-07-16 19:49:05 -0400

    got rid of a period in dropdownitem text.

--- client/src/components/NotificationDropdownItem/NotificationDropdownItem.js
+++ client/src/components/NotificationDropdownItem/NotificationDropdownItem.js
@@ -2,29 +2,24 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Card, Button, Text, NavDropdown } from 'react-bootstrap';
 import { deleteNotification } from '../../utils/API'
 
-import AuthService from '../../utils/auth';
 import UserInfoContext from '../../utils/UserInfoContext'
-
-
-
 
 const NotificationDropdownItem = ({ likerUsername, title, notificationId }) => {
 
     const userData = useContext(UserInfoContext)
+
     const handleDeleteNotification = (notificationId) => {
-        
         console.log("notification Id", notificationId)
         deleteNotification(notificationId)
             .then(() => {
                 userData.getUserData();
-            })
+            });
 
     };
 
     return (
-        <NavDropdown.Item>{likerUsername} liked your post of {title}.<Button onClick={() => handleDeleteNotification(notificationId)}>Oh, word.</Button></NavDropdown.Item>
+        <NavDropdown.Item>{likerUsername} liked your post of {title}<Button onClick={() => handleDeleteNotification(notificationId)}>Oh, word.</Button></NavDropdown.Item>
     )
-
-}
+};
 
 export default NotificationDropdownItem;
