687a13e021082026abf2da27cb75809934e13d47
emi <ejhartline@gmail.com>
2020-11-02 10:19:42 -0500

    changed heroku connection var name (for migration from mlab to atlas)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.DB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
