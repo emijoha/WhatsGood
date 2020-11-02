ff6e05d9805f80aeb83bbbf7b253bffc27d0bcd5
emi <ejhartline@gmail.com>
2020-11-02 11:33:45 -0500

    changed to atlas cluster connection string var (ATLAS_URI) for test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.ATLAS_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
